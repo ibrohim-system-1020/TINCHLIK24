646e3a0e566b02fcfa4e91aff26beb27364e6859
Ibrohim <pahlavonovibrohim34@gmail.com>
2026-08-29 15:13:40 +0500

    Fix ASGI initialization order for Vercel

--- config/asgi.py
+++ config/asgi.py
@@ -4,24 +4,29 @@ ASGI config for config project.
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+from django.core.asgi import get_asgi_application
+
+# Avval Django to'liq ishga tushadi
+django_asgi_app = get_asgi_application()
+
+# Django apps tayyor bo'lgandan keyin models/routing import qilinadi
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.sessions import SessionMiddlewareStack
-from django.core.asgi import get_asgi_application
 
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             SessionMiddlewareStack(
-                AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+                AuthMiddlewareStack(
+                    URLRouter(chat.routing.websocket_urlpatterns)
+                )
             )
         ),
     }
